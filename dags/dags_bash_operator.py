67313abde80bb439b1ac3a6ab994fcb8fec01553
chojungseok <whwndtjr9712@naver.com>
2024-12-23 20:35:13 +0900

    bash_operator dag 업로드

--- dags/dags_bash_operator.py
+++ dags/dags_bash_operator.py
@@ -6,7 +6,7 @@ from airflow.operators.bash import BashOperator
 with DAG(
     dag_id="dags_bash_operator",  ###localhost로 접속했을때 보이는 제목들(파이썬 파일명과는 상관없음 그러나 일치시켜주는게 보기 좋음)
     schedule="0 0 * * *", ##(분, 시, 일, 월, 요일)
-    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"), 
+    start_date=pendulum.datetime(2024, 12, 23, tz="Asia/Seoul"), 
     catchup=False,
     tags=["example", "example2"], ### 처음 들어갔을때 제목(dag_id) 밑에 보이는 파란 글씨들
 ) as dag:
