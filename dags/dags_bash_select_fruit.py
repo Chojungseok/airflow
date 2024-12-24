784febbb6fcb732a2b567100c1ce45d4c5de0d2e
chojungseok <whwndtjr9712@naver.com>
2024-12-25 01:43:45 +0900

    select_fruit,sh 수행하는 dag 업로드

--- dags/dags_bash_select_fruit.py
+++ dags/dags_bash_select_fruit.py
@@ -1,4 +1,4 @@
-from airflow.models.dag import DAG
+from airflow import DAG
 import datetime
 import pendulum
 from airflow.operators.bash import BashOperator
@@ -7,8 +7,7 @@ with DAG(
     dag_id="dags_bash_select_fruit", 
     schedule="10 0 * * 6#1", 
     start_date=pendulum.datetime(2024, 12, 25, tz="Asia/Seoul"), 
-    catchup=False,
-    tags=["example", "example2"], ### 처음 들어갔을때 제목(dag_id) 밑에 보이는 파란 글씨들
+    catchup=False
 ) as dag:
     
     t1_orange = BashOperator(
