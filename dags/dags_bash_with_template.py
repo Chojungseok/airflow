07f95a7c73c4e8ea650304b274947e564f370787
chojungseok <whwndtjr9712@naver.com>
2025-01-01 16:42:29 +0900

    bash_template_dag 업로드

--- dags/dags_bash_with_template.py
+++ dags/dags_bash_with_template.py
@@ -20,7 +20,7 @@ with DAG(
             'START_DATE':'{{data_interval_start | ds}}',
             'END_DATE': '{{data_interval_end | ds}}'
         },
-        bash_command='echo $START_DATE && ehco $END_DATE'
+        bash_command='echo $START_DATE && echo $END_DATE'
 
     )
 
