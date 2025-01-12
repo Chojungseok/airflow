b94408dfa9a0b3369a95164278834e515b91c05f
chojungseok <whwndtjr9712@naver.com>
2025-01-12 10:42:04 +0900

    trigger_operator연습

--- dags/dags_base_branch_operator.py
+++ dags/dags_base_branch_operator.py
@@ -1,52 +1,47 @@
 from airflow import DAG
-import pendulum
-import datetime
 from airflow.operators.python import PythonOperator, BranchPythonOperator
-from airflow.decorators import task
+from airflow.utils.dates import days_ago
+import random
 
+def select_random(**kwargs):
+    item_list = ["A", "B", "C"]
+    selected_item = random.choice(item_list)
+    if selected_item == "A":
+        return 'task_a'
+    else:
+        return ['task_b', 'task_c']
+
+def common_func(selected):
+    print(f"Task {selected} is running")
 
 with DAG(
     dag_id="dags_base_branch_operator",
-    start_date=pendulum.datetime(2024, 12, 25, tz = "Asia/Seoul"),
-    schedule = None,
+    start_date=days_ago(1),
+    schedule_interval=None,
     catchup=False
-)as dag:
-    class CustomBranchOperaotr(BranchPythonOperator):
-        def choose_branch(self, context):
-            import random
+) as dag:
     
-    @task.branch(task_id = 'python_branch_task')
-    def select_random(self, context):
-        import random
-        print(context)
-        item_list = ["A", "B", "C"]
-        selected_item = random.choice(item_list)
-        if selected_item == "A":
-            return 'task_a'
-        elif selected_item in ["B", "C"]:
-            return['task_b', 'task_c']
-
-    custom_branch_operaotr = CustomBranchOperaotr(task_id = 'python_branch_task')
-
-    def common_func(**kwargs):
-        print(kwargs['selected'])
+    branch_task = BranchPythonOperator(
+        task_id='python_branch_task',
+        python_callable=select_random
+    )
 
     task_a = PythonOperator(
-        task_id = 'task_a',
-        python_callable = common_func,
-        op_kwargs = {'selected' : 'A'}
+        task_id='task_a',
+        python_callable=common_func,
+        op_kwargs={'selected': 'A'}
     )
+
     task_b = PythonOperator(
-        task_id = 'task_b',
-        python_callable = common_func,
-        op_kwargs = {'selected' : 'B'}
+        task_id='task_b',
+        python_callable=common_func,
+        op_kwargs={'selected': 'B'}
     )
 
     task_c = PythonOperator(
-        task_id = 'task_c',
-        python_callable = common_func,
-        op_kwargs = {'selected' : 'C'}
+        task_id='task_c',
+        python_callable=common_func,
+        op_kwargs={'selected': 'C'}
     )
 
-    custom_branch_operaotr >> [task_a, task_b, task_c]
-    
+    branch_task >> [task_a, task_b, task_c]
