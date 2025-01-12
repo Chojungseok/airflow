097a7c23dde2207919f113aaa5b0cbeae86c3abc
chojungseok <whwndtjr9712@naver.com>
2025-01-12 22:13:31 +0900

    api_operator 연습

--- dags/dags_seoul_api_sharebike.py
+++ dags/dags_seoul_api_sharebike.py
@@ -10,21 +10,23 @@ with DAG(
     catchup=False
 )as dag:
     '''서울시 공공자전거 대여 정보'''
-    tb_souel_sharebike_info = SeoulApiToCsvOperator(
+    tb_souel_sharebike_info=SeoulApiToCsvOperator(
         task_id = 'tb_souel_sharebike_info',
         dataset_nm='tbCycleRentUseDayInfo',
         path='airflow/files/tbCycleRentUseDayInfo/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash}}',
         file_name='tbCycleRentUseDayInfo.csv',
+        base_dt='{{ data_interval_end.in_timezone("Asia/Seoul") | ds_nodash }}'  # 날짜 자동 설정
+
     )
 
 
-    '''서울시 공공자전거 대여소 현황'''
-    Seoul_share_bike_starion_List = SeoulApiToCsvOperator(
-        task_id = 'Seoul_share_bike_starion_List',
-        dataset_nm='bikeList',
-        path='airflow/files/bikeList/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash}}',
-        file_name='bikeList.csv'
-    )
+    # '''서울시 공공자전거 대여소 현황'''
+    # Seoul_share_bike_starion_List=SeoulApiToCsvOperator(
+    #     task_id = 'Seoul_share_bike_starion_List',
+    #     dataset_nm='bikeList',
+    #     path='airflow/files/bikeList/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash}}',
+    #     file_name='bikeList.csv'
+    # )
 
 
-    tb_souel_sharebike_info >> Seoul_share_bike_starion_List
+    tb_souel_sharebike_info 
